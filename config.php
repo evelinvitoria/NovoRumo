f14215c61a70fc6592e3a2b60dbf1d5897b2fe79
evelinvitoria <114104194+evelinvitoria@users.noreply.github.com>
2025-03-29 15:05:33 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+    define('HOST','localhost');
+    define('USER','root');
+    define('PASSWORD','');
+    define('BASE','cadastroNovoRumo');
+
+    $conn = new mysqli(HOST,USER,PASSWORD,BASE);
+    
+
+?>
